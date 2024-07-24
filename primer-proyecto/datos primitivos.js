d32318b47561edb97b3723fc2de702077986be9d
Larisa Soto Morones <larisotom@hotmail.com>
2024-07-23 23:37:55 -0600

    Add files via upload

---
+++ primer-proyecto/datos primitivos.js
@@ -0,0 +1,14 @@
+//Datos primitivos
+//Number
+18;
+
+//String
+"Hola Mundo";
+'Hola Mundo'
+
+//Booleano
+true;
+false;
+
+null;
+undefined;
---
+++ primer-proyecto/notaciones.js
@@ -0,0 +1,45 @@
+//Notaciones
+    // ; --> Delimitar el final de una linea
+//(NO SIEMPRE SE REQUIERE)
+const a = 1;
+a;
+//-------------------------------------------------
+
+    // . --> Accede a los atributos de un objeto
+// EJEMPLO
+//alumno.edad;
+
+//-------------------------------------------------
+
+    // () --> Parenthesis funciones "ASIGNAR VALORES" 
+/*function promedio (a, b, c){
+    //TODO EL PROCESO DE LA FUNCIÓN
+}
+if (a > b){
+    //PROCESOS
+} */
+
+//---------------------------------------------------
+
+// [] --> Square brackets = listas o arrays
+//DECLARAR
+const ar = [1, 2, 3, 4, 5];
+//ACCEDER
+ar[2]
+console.log(ar[3])
+
+//---------------------------------------------------
+
+// {} --> Curly brackets = crear objetos, funciones y estructuras
+/* const alumno ={
+    edad: 15,
+    estatura: 1.70,
+}
+
+if (a > b){
+    //ESTRUCTURA DE CONTROL
+}
+
+for (const x = 1, 1 = i, i++){
+    //ESTRUCTURA DE CONTROL
+}*/
